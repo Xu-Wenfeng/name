09361328b1e517b3c90f82873f7940d99c7b0c6e
Xu-Wenfeng <2338144108@qq.com>
2025-07-15 16:27:33 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app">
+        <h1>大帅比是谁？</h1>
+        <Person/>
+    </div>
+</template>
+
+<script>
+    import Person from './components/Person.vue'
+    export default  {
+        name:'App',
+        components:{Person}
+    }
+</script>
+
+<style>
+    .app{
+        background-color:#ddd;
+        box-shadow:0 0 10px;
+        padding:10px;
+    }
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="person">
+        <button @click="change">大帅比是谁？</button>
+    </div>
+</template>
+
+<script setup>
+    import {ref} from 'vue'
+    let name = ref('唐少！！')
+    function change(){
+        alert(name.value)
+    }
+    
+</script> 
+
+<style>
+    .person{
+        background-color:skyblue;
+        box-shadow:0 0 10px;
+        padding:20px;
+    }
+    button{
+        margin:0 5px
+    }
+</style>
